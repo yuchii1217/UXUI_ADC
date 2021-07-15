9b22a47a5679aa5803225608d2a74b49a739f85e
estherlim <77757850+elim0201@users.noreply.github.com>
2021-07-14 17:11:31 -0700

    js, footer, community
    added scrollreveal
    footer - 80%
    community created empty space on right

---
+++ js/index.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.upcomingevents', {delay: 200});
+ScrollReveal().reveal('.section-ourguru', {delay: 400});
+ScrollReveal().reveal('.section-offerings', {delay: 400});
+ScrollReveal().reveal('.community-banner', {delay: 400});
+
+ScrollReveal().reveal('.card', {interval: 500});
+ScrollReveal().reveal('.prim-navbar-title', {interval: 400});
